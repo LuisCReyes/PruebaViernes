764b489dbf156ccdec669d67ada8f78ff162cf17
cerf007 <cerf02062007@gmail.com>
2025-05-16 07:46:23 -0600

    Contador de computadoras agregado

--- registro.py
+++ registro.py
@@ -11,6 +11,7 @@ laboratorio1_ocupada = 0    #Contadores necesarios a la hora que ejecute el cicl
 laboratorio2_libre = 0      #Cada uno de ellos corresponde a un laboratorio (ciclo) diferente
 laboratorio2_ocupada = 0
 estado = ""
+n_computadoras = 0
 for laboratorio in range (1,3): 
     # Se supone que todo lo de adentro se repetira hasta 2 veces, dando los laboratorios 
     print(f"==++=="*20)
@@ -20,16 +21,17 @@ for laboratorio in range (1,3):
     for filas in range (1, 6): 
     # Repite el ciclo de for computadoras... 4 veces
         for computadoras in range (1,5): 
+            n_computadoras += 1
         # Como este ciclo esta bajo influencia de el anterior, se supone que serían unas 5*4 veces, es decir 20 veces
             while True: 
                 #En este trato de asegurame que estado es solo L o solo O
-                estado = (input(f"\nIngrese el estado de la computadora [L = LIBRE, O = OCUPADO]: ")) 
+                estado = (input(f"\nIngrese el estado de la computadora {n_computadoras}[L = LIBRE, O = OCUPADO]: ")) 
                 if estado == "L" or estado == "O":  
                 # Si ponen otro valor, les debería saltar el sig mensaje de error
                     break
                 print(f"------"*20)
                 print(f"\nERROR, INGRESE EL VALOR REQUERIDO [L = LIBRE, O = OCUPADO]") 
-                print(f"------"*20)
+                print(f"------ "*20)
                 #Cuando deje d ingresar el valor equivocado, se dentedrá el cilo while (Dejara de aparecer el mensaje de error)
             if estado == "L": 
             #Con esta condición me aseguro el uso del contador para las L
